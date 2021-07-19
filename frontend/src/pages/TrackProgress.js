d20adfe8c3296d909236d648842398c7f72ab585
Vinson Beduya <vinsonemb.151994@gmail.com>
2021-07-19 20:21:44 +1200

    Built initial UI design for track progress page.

--- frontend/src/pages/TrackProgress.js
+++ frontend/src/pages/TrackProgress.js
@@ -1,14 +1,35 @@
-import { makeStyles } from "@material-ui/core";
+import { makeStyles, Button, ThemeProvider, Typography } from "@material-ui/core";
 import { useEffect, useState } from "react";
 import { Route, Switch, useRouteMatch, Link } from "react-router-dom";
 import routineServices from "../services/routineServices";
 import CreateWorkout from "./CreateWorkout";
 import RoutineDetails from "./RoutineDetails";
+import customTheme from "../themes/customTheme";
+import { bgColor, textColor } from "../defaults";
 
 const useStyles = makeStyles((theme) => ({
+    headerContainer: {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "start",
+        border: "1px solid black",
+        marginBottom: 10,
+        background: bgColor,
+        color: textColor
+    },
+    header: {
+        fontFamily: "Impact",
+        letterSpacing: 1.5,
+        padding: 25
+    },
     linksContainer: {
         display: "flex",
         flexDirection: "column"
+    },
+    button: {
+        width: "100%",
+        marginBottom: 10
     }
 }));
 
@@ -27,15 +48,27 @@ const TrackProgress = () => {
     return (
         <Switch>
             <Route exact path={`${path}`}>
-                <h1>Track progress!</h1>
-                <div className={classes.linksContainer}>
-                    {routines.map(routine => <Link key={routine._id} to={`${path}/${routine._id}`}>{path}/{routine.name}</Link>)}
-                    <Link to={`${path}/create-workout`}>{path}/create-workout</Link>
-                </div>
+                <ThemeProvider theme={customTheme.myTheme}>
+                    <div className={classes.headerContainer}>
+                        <Typography variant="h4" className={classes.header}>Track Progress</Typography>
+                    </div>
+                    <div className={classes.linksContainer}>
+                        {routines.map(routine => (
+                            <>
+                                <Link key={routine._id} to={`${path}/${routine._id}`}>
+                                    <Button className={classes.button} variant="outlined" color="primary">{routine.name}</Button>
+                                </Link>
+                            </>
+                        ))}
+                        <Link to={`${path}/create-workout`}>
+                            <Button className={classes.button} variant="contained" color="primary">Create Workout</Button>
+                        </Link>
+                    </div>
+                </ThemeProvider>
             </Route>
             {routines.map((routine, routineIndex) => (
                 <Route key={routine._id} path={`${path}/${routine._id}`}>
-                    <RoutineDetails routine={routine} routineIndex={routineIndex} routines={routines} setRoutines={setRoutines}/>
+                    <RoutineDetails routine={routine} routineIndex={routineIndex} routines={routines} setRoutines={setRoutines} />
                 </Route>
             ))}
             <Route path={`${path}/create-workout`}>
