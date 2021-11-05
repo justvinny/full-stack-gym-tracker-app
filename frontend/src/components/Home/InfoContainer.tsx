ea1879010c3431e2366068c78d892b652c3d7c47
Vinson Beduya <vinsonemb.151994@gmail.com>
2021-11-05 16:26:04 +1300

    Added function to extract initials from name.

--- frontend/src/components/Home/InfoContainer.tsx
+++ frontend/src/components/Home/InfoContainer.tsx
@@ -9,49 +9,67 @@ interface Props {
   aboutMe: string;
 }
 
-const InfoContainer = ({ name, age, height, aboutMe }: Props) => (
-  <Grid
-    item
-    xs={12}
-    sx={{
-      padding: "16px",
-      boxShadow: "0px 0px 5px 2px",
-      marginBottom: "12px",
-    }}
-  >
-    <ContainerHeading label="Basic Information" />
-    <Box
+const InfoContainer = ({ name, age, height, aboutMe }: Props) => {
+  const getInitials = () => {
+    const nameArr = name.split(" ");
+
+    switch (nameArr.length) {
+      case 0: {
+        return "A";
+      }
+      case 1: {
+        return nameArr[0].charAt(0);
+      }
+      default: {
+        return nameArr[0].charAt(0) + nameArr[nameArr.length - 1].charAt(0);
+      }
+    }
+  };
+
+  return (
+    <Grid
+      item
+      xs={12}
       sx={{
-        display: "flex",
-        flexDirection: { xs: "column", sm: "row" },
-        alignItems: { xs: "center" },
+        padding: "16px",
+        boxShadow: "0px 0px 5px 2px",
+        marginBottom: "12px",
       }}
     >
-      <Avatar
-        sx={{
-          width: { xs: "150px" },
-          height: { xs: "150px" },
-          fontSize: { xs: 75 },
-          bgcolor: bgColor,
-        }}
-      >
-        VB
-      </Avatar>
+      <ContainerHeading label="Basic Information" />
       <Box
         sx={{
           display: "flex",
-          flexDirection: "column",
-          marginTop: { xs: "8px", sm: 0 },
-          marginLeft: { xs: 0, sm: "8px" },
+          flexDirection: { xs: "column", sm: "row" },
+          alignItems: { xs: "center" },
         }}
       >
-        <Box>Name: {name}</Box>
-        <Box>Age: {age}</Box>
-        <Box>Height: {height}cm</Box>
-        <Box sx={{ textAlign: "justify" }}>About Me: {aboutMe}</Box>
+        <Avatar
+          sx={{
+            width: { xs: "150px" },
+            height: { xs: "150px" },
+            fontSize: { xs: 75 },
+            bgcolor: bgColor,
+          }}
+        >
+          {getInitials()}
+        </Avatar>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            marginTop: { xs: "8px", sm: 0 },
+            marginLeft: { xs: 0, sm: "8px" },
+          }}
+        >
+          <Box>Name: {name}</Box>
+          <Box>Age: {age}</Box>
+          <Box>Height: {height}cm</Box>
+          <Box sx={{ textAlign: "justify" }}>About Me: {aboutMe}</Box>
+        </Box>
       </Box>
-    </Box>
-  </Grid>
-);
+    </Grid>
+  );
+};
 
 export default InfoContainer;
